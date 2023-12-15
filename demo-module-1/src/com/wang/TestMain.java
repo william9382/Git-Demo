ea6e9753c273f5e6d1a577445dc042b82ea35429
william9382 <3188337253@qq.com>
2023-12-15 11:42:38 +0800

    test分枝修改

--- demo-module-1/src/com/wang/TestMain.java
+++ demo-module-1/src/com/wang/TestMain.java
@@ -3,6 +3,6 @@ package com.wang;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("haha");
-        System.out.println("test");
+        System.out.println("testbranch");
     }
 }
